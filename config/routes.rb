8fbd63cf9cd30a82d289370dad9099a1eadf4aa3
Brian Chang <changbhc@gmail.com>
2021-09-07 17:27:21 -0700

    feat: add hello_world route as get and root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'home#hello_world'
+  
+  get 'hello_world', to: 'home#hello_world'
 end
