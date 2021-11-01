0cda06ba60f4477fb4a5b4bb3701044e095e3952
Brian Chang <changbhc@gmail.com>
2021-10-31 22:10:13 -0700

    setup: configure rspec

--- config/application.rb
+++ config/application.rb
@@ -33,3 +33,14 @@ module CoachingRailsBlog
     config.generators.system_tests = nil
   end
 end
+
+config.generators do |g|
+  g.test_framework :rspec,
+    fixtures: true,
+    view_specs: false,
+    helper_specs: false,
+    routing_specs: false,
+    controller_specs: true,
+    request_specs: false
+  g.fixture_replacement :factory_girl, dir: "spec/factories"
+end
