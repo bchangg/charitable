6ecc7a53bd925d004d673eccaefde1d494b3472f
Brian Chang <changbhc@gmail.com>
2021-11-04 16:09:25 -0700

    setup: update gems for tests

--- config/application.rb
+++ config/application.rb
@@ -34,13 +34,10 @@ module CoachingRailsBlog
 
     config.generators do |g|
       g.test_framework :rspec,
-        fixtures: true,
         view_specs: false,
         helper_specs: false,
-        routing_specs: false,
-        controller_specs: true,
-        request_specs: false
-      g.fixture_replacement :factory_bot, dir: "spec/factories"
+        routing_specs: false
+      # g.fixture_replacement :factory_bot, dir: "spec/factories"
     end
   end
 end
