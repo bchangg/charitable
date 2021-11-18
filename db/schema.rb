d17273bb9575749ce1726b36b786e7be4359ae84
Brian Chang <changbhc@gmail.com>
2021-11-17 16:09:15 -0800

    feat: migrate jobs into db

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_11_18_000056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "title"
+    t.string "location"
+    t.text "description"
+    t.date "date"
+    t.time "time"
+    t.integer "openings"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
